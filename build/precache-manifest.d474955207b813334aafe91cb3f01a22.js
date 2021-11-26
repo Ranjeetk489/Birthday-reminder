4fd800a0f5f4ca0dc19c67ffba8d89761431828e
Ranjeetk489 <49064698+Ranjeetk489@users.noreply.github.com>
2021-11-27 02:28:40 +0530

    Added files via upload

---
+++ build/precache-manifest.d474955207b813334aafe91cb3f01a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae0c02d3036d5f4d51371c8b490dd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42c0f3b22c9ca59680a4",
+    "url": "/static/css/main.701da124.chunk.css"
+  },
+  {
+    "revision": "562fb19fe3d711f428ca",
+    "url": "/static/js/2.077b7fb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.077b7fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c0f3b22c9ca59680a4",
+    "url": "/static/js/main.3dcf50d8.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
